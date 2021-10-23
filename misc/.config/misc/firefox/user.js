ad7c1ab29921cda43321f5254db2cc8fe842c583
Shivanshu Kumar <60311696+shivanshukmr@users.noreply.github.com>
2021-10-23 14:33:27 +0530

    feat(firefox): resist fingerprinting
    Also,
    	firefox: change accel key to default (ctrl)

--- misc/.config/misc/firefox/user.js
+++ misc/.config/misc/firefox/user.js
@@ -1,7 +1,8 @@
 /* alt key as accelKey */
-user_pref("ui.key.accelKey", 18);
 user_pref("ui.key.menuAccessKey", 0);
 
 /* Pocket and firefox account disabled */
 user_pref("identity.fxaccounts.enabled", false);
 user_pref("extensions.pocket.enabled", false);
+
+user_pref("privacy.resistFingerprinting", true);
