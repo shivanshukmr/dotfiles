b68998010db8a28e97ed7a9000b965ab948c4aab
Shivanshu Kumar <60311696+shivanshukmr@users.noreply.github.com>
2021-11-07 12:52:34 +0530

    feat(firefox): enable hardware acceleration
    Using hardware acceleration using VAAPI,
    required packages:
    	mesa-dri
    	mesa-vaapi
    	libvautils (for `vainfo`)
    	libavcodecs (codecs for playing videos)

--- misc/.config/misc/firefox/user.js
+++ misc/.config/misc/firefox/user.js
@@ -6,3 +6,10 @@ user_pref("identity.fxaccounts.enabled", false);
 user_pref("extensions.pocket.enabled", false);
 
 user_pref("privacy.resistFingerprinting", true);
+
+/* Hardware acceleration */
+user_pref("gfx.webrender.all", true);
+user_pref("media.ffmpeg.vaapi.enabled", true);
+user_pref("media.ffvpx.enabled", false);
+user_pref("media.navigator.mediadatadecoder_vpx_enabled", true);
+user_pref("media.rdd-vpx.enabled", false);
