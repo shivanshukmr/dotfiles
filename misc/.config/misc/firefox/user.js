905998fd2cfa2f1251e469d7e9854a0029628ccc
Shivanshu Kumar <shivanshukmr@protonmail.com>
2021-12-09 11:03:25 +0530

    feat(firefox): enable fission
    https://wiki.mozilla.org/Project_Fission

--- misc/.config/misc/firefox/user.js
+++ misc/.config/misc/firefox/user.js
@@ -1,15 +1,18 @@
-/* alt key as accelKey */
+// alt key as accelKey
 user_pref("ui.key.menuAccessKey", 0);
 
-/* Pocket and firefox account disabled */
+// Pocket and firefox account disabled
 user_pref("identity.fxaccounts.enabled", false);
 user_pref("extensions.pocket.enabled", false);
 
 user_pref("privacy.resistFingerprinting", true);
 
-/* Hardware acceleration */
+// Hardware acceleration
 user_pref("gfx.webrender.all", true);
 user_pref("media.ffmpeg.vaapi.enabled", true);
 user_pref("media.ffvpx.enabled", false);
 user_pref("media.navigator.mediadatadecoder_vpx_enabled", true);
 user_pref("media.rdd-vpx.enabled", false);
+
+// Fission
+user_pref("fission.autostart", true);
